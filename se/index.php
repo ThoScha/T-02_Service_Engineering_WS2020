ac6d2c45d0c3f375a4c7a155e7add707b65a7644
MrGatzi <martingatzweiler@gmail.com>
2020-11-14 00:16:15 +0100

    Update #2
    changed the cullom ot the correct order.
    added headers for tabels.
    added different input typs
    added some data.
    updated functions.
    
    missing date-time functions and behaivior.

--- se/index.php
+++ se/index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1>MfCDSD</h1>
-<h2>My first Corona Daten Sauger Dashboard :P</h2>
+<h2>My first Corona Daten Sauger Dashboard :P #sloppy Jessica </h2>
 
 <?php
 require __DIR__ . '/vendor/autoload.php';
@@ -24,11 +24,14 @@ if (isset($_POST['update_button'])) {
 
     $userID=$_POST['userID'];
     $nr=array_search($userID, array_column($db["persons"], 'id'));
-    $fn=$_POST['firstname'];
-    $ln=$_POST['lastname'];
 
-    $db["persons"][$nr]["firstname"]=$fn;
-    $db["persons"][$nr]["lastname"]=$ln;
+    $db["persons"][$nr]["age"]=$_POST['age'];
+    $db["persons"][$nr]["gender"]=$_POST['gender'];
+    $db["persons"][$nr]["preill"]=$_POST['preill'];
+    $db["persons"][$nr]["district"]=$_POST['district'];
+    $db["persons"][$nr]["state"]=$_POST['state'];
+ 
+
     file_put_contents('test.json', json_encode($db));
 
 } else if (isset($_POST['delete_button'])) {
@@ -42,8 +45,11 @@ if (isset($_POST['update_button'])) {
 
     $object = new stdClass();
     $object->id = $id;
-    $object->firstname = $_POST['firstname'];
-    $object->lastname = $_POST['lastname'];
+    $object->age = $_POST['age'];
+    $object->gender = $_POST['gender'];
+    $object->preill = $_POST['preill'];
+    $object->district = $_POST['district'];
+    $object->state = $_POST['state'];
     array_push($db["persons"], $object);
 
     file_put_contents('test.json', json_encode($db));
